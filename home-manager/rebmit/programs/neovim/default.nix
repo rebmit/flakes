acca598b9e6bc2ad5f82faac5b54b784a3acb8c1
rebmit <i@rebmit.moe>
2024-03-06 23:11:57 +0800

    home-manager/rebmit: use vimPlugins.nvchad

--- home-manager/rebmit/programs/neovim/default.nix
+++ home-manager/rebmit/programs/neovim/default.nix
@@ -2,5 +2,19 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+
+  xdg.configFile = {
+    "nvim" = {
+      source = pkgs.vimPlugins.nvchad;
+      recursive = true;
+    };
+
+    "nvim/lua/custom" = {
+      source = ./nvchad;
+      recursive = true;
+    };
   };
 }
