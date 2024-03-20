e0871757cf752b59344905b939582581671cdf09
rebmit <i@rebmit.moe>
2024-03-20 19:09:56 +0800

    home-manager/rebmit: drop nvchad

--- home-manager/rebmit/programs/neovim/default.nix
+++ home-manager/rebmit/programs/neovim/default.nix
@@ -1,20 +1,28 @@
-{pkgs, ...}: {
+{
+  mylib,
+  pkgs,
+  ...
+}: {
+  imports = mylib.getItemPaths ./. "default.nix";
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     vimAlias = true;
     vimdiffAlias = true;
-  };
 
-  xdg.configFile = {
-    "nvim" = {
-      source = pkgs.vimPlugins.nvchad;
-      recursive = true;
-    };
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+      nvim-cmp
+      cmp-nvim-lsp
+      catppuccin-nvim
+      luasnip
+      which-key-nvim
+      leap-nvim
+    ];
 
-    "nvim/lua/custom" = {
-      source = ./nvchad;
-      recursive = true;
-    };
+    extraConfig = ''
+      :source ${./nvim.lua}
+    '';
   };
 }
