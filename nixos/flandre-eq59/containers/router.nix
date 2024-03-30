29afc209007c6ea79655db980ddc06b22a15aeb7
rebmit <i@rebmit.moe>
2024-03-30 23:22:40 +0800

    nixos/flandre-eq59: enable xl2tpd for pppol2tp

--- nixos/flandre-eq59/containers/router.nix
+++ nixos/flandre-eq59/containers/router.nix
@@ -1,12 +1,23 @@
-{ lib, config, ... }: {
+{ lib, pkgs, config, ... }: {
   custom.containers."router" = {
     autoStart = true;
     privateNetwork = true;
     ephemeral = true;
     extraVeths."router-lan".hostBridge = "brlan";
     extraVeths."router-wan".hostBridge = "brwan";
-    bindMounts."${config.sops.templates.router-xl2tpd.path}".isReadOnly = true;
-    bindMounts."${config.sops.templates.router-pppoptions.path}".isReadOnly = true;
+    bindMounts."${config.sops.templates.router-xl2tpd.path}" = { };
+    bindMounts."${config.sops.templates.router-pppoptions.path}" = { };
+    bindMounts."/dev/ppp" = {
+      isReadOnly = false;
+      useRootIdMap = false;
+    };
+    privileged = true;
+    allowedDevices = [
+      {
+        modifier = "rw";
+        node = "/dev/ppp";
+      }
+    ];
     config = {
       networking.useHostResolvConf = lib.mkForce false;
 
@@ -86,6 +97,46 @@
           };
         };
       };
+
+      systemd.services.xl2tpd-client =
+        let
+          xl2tpd-ppp-wrapped = pkgs.stdenv.mkDerivation {
+            name = "xl2tpd-ppp-wrapped";
+            phases = [ "installPhase" ];
+            nativeBuildInputs = with pkgs; [ makeWrapper ];
+            installPhase = ''
+              mkdir -p $out/bin
+
+              makeWrapper ${pkgs.ppp}/sbin/pppd $out/bin/pppd \
+                --set LD_PRELOAD    "${pkgs.libredirect}/lib/libredirect.so" \
+                --set NIX_REDIRECTS "/etc/ppp=/etc/xl2tpd/ppp"
+
+              makeWrapper ${pkgs.xl2tpd}/bin/xl2tpd $out/bin/xl2tpd \
+                --set LD_PRELOAD    "${pkgs.libredirect}/lib/libredirect.so" \
+                --set NIX_REDIRECTS "${pkgs.ppp}/sbin/pppd=$out/bin/pppd"
+            '';
+          };
+        in
+        {
+          description = "xl2tpd client";
+
+          requires = [ "network-online.target" ];
+          wantedBy = [ "multi-user.target" ];
+
+          preStart = ''
+            mkdir -p /run/xl2tpd
+            chown root:root /run/xl2tpd
+            chmod 700       /run/xl2tpd
+          '';
+
+          serviceConfig = {
+            ExecStart = "${xl2tpd-ppp-wrapped}/bin/xl2tpd -D -c ${config.sops.templates.router-xl2tpd.path} -p /run/xl2tpd/pid -C /run/xl2tpd/control";
+            KillMode = "process";
+            Restart = "on-success";
+            Type = "simple";
+            PIDFile = "/run/xl2tpd/pid";
+          };
+        };
 
       services.resolved.enable = true;
 
