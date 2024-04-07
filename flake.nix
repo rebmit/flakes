71a8a4667dc66fa04615f6e7e2a06648208f95ba
rebmit <i@rebmit.moe>
2024-04-08 01:28:30 +0800

    inputs: use hyprland v0.38.1

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.38.0";
+      url = "github:hyprwm/Hyprland/v0.38.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
