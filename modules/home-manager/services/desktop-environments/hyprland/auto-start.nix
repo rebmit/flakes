b913a2827a3ef4c28a20a84210f2daef7cde5285
rebmit <i@rebmit.moe>
2024-04-10 18:56:19 +0800

    home-manager/hyprland: workaround for swww issue
    https://github.com/LGFae/swww/issues/233

--- modules/home-manager/services/desktop-environments/hyprland/auto-start.nix
+++ modules/home-manager/services/desktop-environments/hyprland/auto-start.nix
@@ -29,7 +29,7 @@ in
   wayland.windowManager.hyprland.extraConfig = ''
     # https://discourse.nixos.org/t/clicked-links-in-desktop-apps-not-opening-browers/29114/4
     exec-once = systemctl --user import-environment PATH && systemctl --user restart xdg-desktop-portal.service
-    exec-once = swww init
+    exec-once = swww-daemon --format xrgb
     exec-once = systemd-run-app ags
     exec-once = ${scratchpadHelper}/bin/hyprland-scratchpad-helper
     exec-once = systemd-run-app ${cfg.terminal.startupCommand}
