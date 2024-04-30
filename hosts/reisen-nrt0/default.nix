4cb86c50c504d1861ba48c2ba890e17189126a04
rebmit <i@rebmit.moe>
2024-05-01 03:09:22 +0800

    hosts/reisen-nrt0: init

---
+++ hosts/reisen-nrt0/default.nix
@@ -0,0 +1,20 @@
+{ self, mylib, ... }: {
+  imports =
+    [
+      self.nixosModules.default
+    ]
+    ++ (mylib.getItemPaths ./. "default.nix");
+
+  custom = {
+    baseline.enable = true;
+    cloud.linode.enable = true;
+    system.disko.btrfs-bios-common = {
+      enable = true;
+      device = "/dev/sda";
+    };
+  };
+
+  networking.hostName = "reisen-nrt0";
+
+  system.stateVersion = "23.11";
+}
---
+++ outputs/x86_64-linux/src/reisen-nrt0.nix
@@ -0,0 +1,25 @@
+{ inputs
+, lib
+, system
+, genSpecialArgs
+, myvars
+, mylib
+, ...
+}:
+let
+  name = "reisen-nrt0";
+  tags = [ "reisen" "infra" ];
+  baseModules = {
+    nixosModules = map mylib.relativeToRoot [
+      "hosts/${name}"
+    ];
+  };
+  systemArgs = baseModules // { inherit inputs lib system genSpecialArgs myvars; };
+in
+{
+  nixosConfigurations."${name}" = mylib.nixosSystem systemArgs;
+
+  colmena."${name}" = mylib.colmenaSystem (systemArgs // { inherit tags; });
+
+  packages."${name}" = inputs.self.nixosConfigurations."${name}".config.formats.iso;
+}
