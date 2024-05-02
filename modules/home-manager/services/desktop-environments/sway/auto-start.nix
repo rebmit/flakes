e5216eb2d033b5f49ea14589b2e0ecdc55e43aea
rebmit <i@rebmit.moe>
2024-05-03 03:32:24 +0800

    home-manager/sway: drop ags auto-start

--- modules/home-manager/services/desktop-environments/sway/auto-start.nix
+++ modules/home-manager/services/desktop-environments/sway/auto-start.nix
@@ -6,7 +6,6 @@ in
   wayland.windowManager.sway.config.startup = [
     { command = "systemctl --user import-environment PATH && systemctl --user restart xdg-desktop-portal.service"; }
     { command = "swww-daemon --format xrgb"; }
-    { command = "systemd-run-app ags"; }
     { command = "systemd-run-app ${cfg.terminal.startupCommand}"; }
     { command = "systemd-run-app ${cfg.browser.startupCommand}"; }
   ];
