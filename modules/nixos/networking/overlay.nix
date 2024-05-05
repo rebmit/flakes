786d4a061aa32dc7658814f7ada840f01210666e
rebmit <i@rebmit.moe>
2024-05-05 21:51:01 +0800

    nixos/networking: add wireguard options and presets for overlay network

--- modules/nixos/networking/overlay.nix
+++ modules/nixos/networking/overlay.nix
@@ -2,6 +2,30 @@
 with lib;
 let
   cfg = config.custom.networking.overlay;
+  peerOpts = { ... }: {
+    options = {
+      publicKey = mkOption {
+        type = types.str;
+        description = "public key for the remote node";
+      };
+      sendPort = mkOption {
+        type = types.int;
+        description = "listen port on the local node for the peer-to-peer tunnel";
+      };
+      addressFamily = mkOption {
+        type = types.enum [ "ip4" "ip6" ];
+        description = "address family of the given address";
+      };
+      endpoint = mkOption {
+        type = types.nullOr types.str;
+        description = "publicly accessible endpoint for the remote peer";
+      };
+      persistentKeepalive = mkOption {
+        type = types.nullOr types.int;
+        description = "the interval of keepalive packet to keep a stateful firewall valid persistently";
+      };
+    };
+  };
 in
 {
   options.custom.networking.overlay = {
@@ -9,6 +33,33 @@ in
       declarative overlay network based on wireguard, heavily inspired by
       <https://github.com/NickCao/flakes/blob/master/modules/gravity/default.nix>
     '';
+    wireguard = {
+      enable = mkEnableOption "wireguard";
+      privateKeyPath = mkOption {
+        type = types.str;
+        description = "private key path for the local node";
+      };
+      mtu = mkOption {
+        type = types.int;
+        default = 1400;
+        description = "interface mtu";
+      };
+      firewallMark = mkOption {
+        type = types.int;
+        default = 447;
+        description = "wireguard fwmark for all interfaces";
+      };
+      interfacePrefix = mkOption {
+        type = types.str;
+        default = "ranet";
+        description = "prefix of interface name";
+      };
+      peers = mkOption {
+        type = with types; listOf (submodule peerOpts);
+        default = { };
+        description = "remote peers of the local node";
+      };
+    };
     table = mkOption {
       type = types.int;
       default = 2000;
@@ -93,14 +144,59 @@ in
       }
       (mkIf (cfg.preset) (
         let
+          overlayNetwork = myvars.networks.overlayNetwork;
           hostName = config.networking.hostName;
+          getAddrByFamily = nodeName: addressFamily:
+            if addressFamily == "ip4" then
+              overlayNetwork.nodes."${nodeName}".meta.wireguard.publicIpv4
+            else
+              overlayNetwork.nodes."${nodeName}".meta.wireguard.publicIpv6;
+          links = builtins.filter
+            (value: value.srcName == hostName)
+            (
+              lists.imap0
+                (index: value:
+                  rec {
+                    inherit (value) addressFamily;
+                    sendPort = overlayNetwork.meta.wireguard.basePort + index;
+                    persistentKeepalive =
+                      let
+                        srcAddr = getAddrByFamily value.srcName addressFamily;
+                        destAddr = getAddrByFamily value.destName addressFamily;
+                      in
+                      if srcAddr == null || destAddr == null then 25 else null;
+                  } // (if (value.destName == hostName) then {
+                    srcName = value.destName;
+                    destName = value.srcName;
+                  } else {
+                    inherit (value) srcName destName;
+                  })
+                )
+                myvars.networks.overlayNetwork.links
+            );
+          peers = builtins.map
+            (link: rec {
+              inherit (link) sendPort addressFamily persistentKeepalive;
+              publicKey = overlayNetwork.nodes."${link.destName}".meta.wireguard.publicKey;
+              endpoint =
+                let
+                  address = getAddrByFamily link.destName addressFamily;
+                in
+                if address == null then null else "${address}:${toString sendPort}";
+            })
+            links;
         in
         {
           custom.networking.overlay = {
             address = [
-              (myvars.networks.overlayNetwork.nodes."${hostName}".ipv4)
-              (myvars.networks.overlayNetwork.nodes."${hostName}".ipv6)
+              (overlayNetwork.nodes."${hostName}".ipv4)
+              (overlayNetwork.nodes."${hostName}".ipv6)
             ];
+            wireguard = {
+              enable = true;
+              inherit (overlayNetwork.meta.wireguard) mtu interfacePrefix firewallMark;
+              inherit peers;
+            };
           };
         }
       ))
